6e207932da5306e344b1f72a76683d655881e6b4
atary <atakanaral@gmail.com>
2014-07-03 15:48:02 +0300

    Initial

--- RalloCloud/src/rallocloud/RalloCloud.java
+++ RalloCloud/src/rallocloud/RalloCloud.java
@@ -16,7 +16,7 @@ public class RalloCloud {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("wtf");
     }
     
 }
