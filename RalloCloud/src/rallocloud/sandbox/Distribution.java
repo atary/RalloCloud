c74ab0dd4f7d909995110d6e1fbf6bf4c3e483c2
Atakan <atakanaral@gmail.com>
2015-01-14 02:31:09 +0200

    distribution in sandbox

---
+++ RalloCloud/src/rallocloud/sandbox/Distribution.java
@@ -0,0 +1,22 @@
+package rallocloud.sandbox;
+
+import org.apache.commons.math3.distribution.*;
+
+/**
+ *
+ * @author Atakan
+ */
+public class Distribution {
+
+    public static void main(String[] args) {
+        PoissonDistribution p = new PoissonDistribution(5); //VM sayısı için
+        for (int i = 0; i < 10; i++) {
+            System.out.println(p.sample());
+        }
+
+        UniformRealDistribution u = new UniformRealDistribution(0, 200);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(u.sample());
+        }
+    }
+}
