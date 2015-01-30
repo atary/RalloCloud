af6d25cdd2c184f3a760bbe5998609e1736f1983
Atakan <atakanaral@gmail.com>
2015-01-30 14:37:32 +0200

    batch clears and creates the /out folder

--- RalloCloud/run.bat
+++ RalloCloud/run.bat
@@ -1,3 +1,5 @@
+RD /S /Q dist\out
+MKDIR dist\out
 FOR /L %%G IN (1,1,30) DO START /WAIT java -jar dist/RalloCloud.jar AFF
 FOR /L %%G IN (1,1,30) DO START /WAIT java -jar dist/RalloCloud.jar ANF
 FOR /L %%G IN (1,1,30) DO START /WAIT java -jar dist/RalloCloud.jar LBG
