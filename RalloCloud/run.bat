f4137ff3bac22b6e8003c56b1263dd5c7dc52667
Atakan <atakanaral@gmail.com>
2015-05-04 02:00:17 +0300

    SNW instead of AFF

--- RalloCloud/run.bat
+++ RalloCloud/run.bat
@@ -2,7 +2,7 @@ set vmRAM=%1
 set vmBW=%2
 set vmNUM=%3
 MKDIR dist\out
-FOR /L %%G IN (1,1,30) DO START /B /WAIT java -jar dist/RalloCloud.jar AFF %vmRAM% %vmBW% %vmNUM% %%G/30
+FOR /L %%G IN (1,1,30) DO START /B /WAIT java -jar dist/RalloCloud.jar SNW %vmRAM% %vmBW% %vmNUM% %%G/30
 FOR /L %%G IN (1,1,30) DO START /B /WAIT java -jar dist/RalloCloud.jar ANF %vmRAM% %vmBW% %vmNUM% %%G/30
 FOR /L %%G IN (1,1,30) DO START /B /WAIT java -jar dist/RalloCloud.jar LBG %vmRAM% %vmBW% %vmNUM% %%G/30
 FOR /L %%G IN (1,1,30) DO START /B /WAIT java -jar dist/RalloCloud.jar LFF %vmRAM% %vmBW% %vmNUM% %%G/30
