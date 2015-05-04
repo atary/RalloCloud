71b9c0087fc7ce5ae85e1e4026e3f497afbea90c
Atakan <atakanaral@gmail.com>
2015-05-04 03:19:09 +0300

    more results, batch update

--- RalloCloud/run.bat
+++ RalloCloud/run.bat
@@ -1,11 +1,12 @@
 set vmRAM=%1
 set vmBW=%2
 set vmNUM=%3
+set vmNW=%4
 MKDIR dist\out
-FOR /L %%G IN (1,1,30) DO START /B /WAIT java -jar dist/RalloCloud.jar SNW %vmRAM% %vmBW% %vmNUM% %%G/30
-FOR /L %%G IN (1,1,30) DO START /B /WAIT java -jar dist/RalloCloud.jar ANF %vmRAM% %vmBW% %vmNUM% %%G/30
-FOR /L %%G IN (1,1,30) DO START /B /WAIT java -jar dist/RalloCloud.jar LBG %vmRAM% %vmBW% %vmNUM% %%G/30
-FOR /L %%G IN (1,1,30) DO START /B /WAIT java -jar dist/RalloCloud.jar LFF %vmRAM% %vmBW% %vmNUM% %%G/30
-FOR /L %%G IN (1,1,30) DO START /B /WAIT java -jar dist/RalloCloud.jar RAN %vmRAM% %vmBW% %vmNUM% %%G/30
-FOR /L %%G IN (1,1,30) DO START /B /WAIT java -jar dist/RalloCloud.jar TBF %vmRAM% %vmBW% %vmNUM% %%G/30
-FOR /L %%G IN (1,1,30) DO START /B /WAIT java -jar dist/RalloCloud.jar LNF %vmRAM% %vmBW% %vmNUM% %%G/30
+FOR /L %%G IN (1,1,30) DO START /B /WAIT java -jar dist/RalloCloud.jar SNW %vmRAM% %vmBW% %vmNUM% %vmNW% %%G/30
+FOR /L %%G IN (1,1,30) DO START /B /WAIT java -jar dist/RalloCloud.jar ANF %vmRAM% %vmBW% %vmNUM% %vmNW% %%G/30
+FOR /L %%G IN (1,1,30) DO START /B /WAIT java -jar dist/RalloCloud.jar LBG %vmRAM% %vmBW% %vmNUM% %vmNW% %%G/30
+FOR /L %%G IN (1,1,30) DO START /B /WAIT java -jar dist/RalloCloud.jar LFF %vmRAM% %vmBW% %vmNUM% %vmNW% %%G/30
+FOR /L %%G IN (1,1,30) DO START /B /WAIT java -jar dist/RalloCloud.jar RAN %vmRAM% %vmBW% %vmNUM% %vmNW% %%G/30
+FOR /L %%G IN (1,1,30) DO START /B /WAIT java -jar dist/RalloCloud.jar TBF %vmRAM% %vmBW% %vmNUM% %vmNW% %%G/30
+FOR /L %%G IN (1,1,30) DO START /B /WAIT java -jar dist/RalloCloud.jar LNF %vmRAM% %vmBW% %vmNUM% %vmNW% %%G/30
